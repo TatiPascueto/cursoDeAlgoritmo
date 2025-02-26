9638df6c00ac738a0e1496d7f21b2d1d30c2e9f7
Peregrine <137233198+TatiPascueto@users.noreply.github.com>
2025-02-26 19:46:25 +0000

    Consertando programa de login

--- aulas/aula 3/login.c
+++ aulas/aula 3/login.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int main() {
-  int cpf, senha;
+  
+  int senha;
+  char cpf[12];
 
   printf("Digite seu CPF: ");
-  scanf("%d", &cpf);
+  fgets (cpf, 12, stdin); 
 
   printf("Digite sua senha: ");
-    scanf("%d", &senha);
+  scanf ("%d", &senha);
 
-  printf("Seu CPF é %d e sua senha é %d.\n", cpf, senha);
+  printf("Seu CPF é %s\n", cpf);
+  printf("sua senha é %d\n", senha);
 
   return 0;
 }
